7fa2f3c950e3c91654dc2207b81109efa52cddf4
Babakoki <s2422055@stu.musahino-u.ac.jp>
2024-11-26 23:46:14 +0900

    サイズを変更

--- calc.py
+++ calc.py
@@ -39,7 +39,7 @@ class CalculatorApp(ft.Container):
         self.reset()
 
         self.result = ft.Text(value="0", color=ft.colors.WHITE, size=20)
-        self.width = 350
+        self.width = 550
         self.bgcolor = ft.colors.BLACK
         self.border_radius = ft.border_radius.all(20)
         self.padding = 20
